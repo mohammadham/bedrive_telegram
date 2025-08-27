62db745640d97f8f82f4e18ae68051b522b80a55
Mohammad <mmm.2015.ma60@gmail.com>
2025-08-27 12:15:06 +0330

    dynamic disc problem

--- common/foundation/src/Files/Providers/DynamicStorageDiskProvider.php
+++ common/foundation/src/Files/Providers/DynamicStorageDiskProvider.php
@@ -37,7 +37,7 @@ class DynamicStorageDiskProvider extends ServiceProvider
 
     private function resolveDisk(string $type, array $initialConfig): \League\Flysystem\Filesystem
     {
-        $driverName = config("common.site.{$type}_disk_driver") ?? 'local';
+              $driverName = config("common.site.{$type}_disk_driver") ?? 'local';
         $config = array_merge(
             $initialConfig,
             config("services.$driverName") ?? [],
@@ -65,6 +65,6 @@ class DynamicStorageDiskProvider extends ServiceProvider
         $dynamicConfigKey = "{$type}_{$driverName}";
         Config::set("filesystems.disks.{$dynamicConfigKey}", $config);
 
-        return Storage::build($dynamicConfigKey);
+        return Storage::disk($dynamicConfigKey);
     }
 }
